1f3958ff24baf68003552d1301e1bb839656fe8f
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-09-22 18:15:02 +0200

    [Plugins/sqlite] Adapt to new calling convention

--- src/sqlite/net/instant/plugin/sqlite/SQLitePlugin.java
+++ src/sqlite/net/instant/plugin/sqlite/SQLitePlugin.java
@@ -1,9 +1,10 @@
 package net.instant.plugin.sqlite;
 
 import net.instant.api.API1;
+import net.instant.api.PluginData;
 
 public class SQLitePlugin {
 
-    public static void initInstantPlugin1(API1 api) {}
+    public static void initInstantPlugin1(API1 api, PluginData data) {}
 
 }
