39bb712c8d7e66574afbf4a73a7e59b58db84a0b
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-03-21 15:25:37 +0100

    [Plugins/embed-images] Commit size change in a controlled way
    Previously, a newly-loaded image would resize itself at some random
    point in time, thus displacing the input bar down annoyingly.
    
    Requires CylonicRaider/Instant@45662fc4668a90334f5c65f267756fc95c6382c9
    to work properly (but degrades gracefully if not available).

--- src/embed-images/static/embed-images.js
+++ src/embed-images/static/embed-images.js
@@ -2,6 +2,21 @@
 /* Instant image embedder functionality */
 
 Instant.plugins.mailbox('embed-images').handle(function(data) {
+  function finishLoading(event) {
+    var img = event.target;
+    if (! img.classList.contains('loading')) return;
+    var restore = Instant.input.saveScrollState();
+    img.classList.remove('loading');
+    restore();
+  }
+  function checkSize(img) {
+    if (! img.classList.contains('loading')) return;
+    if (img.naturalHeight != 0) {
+      finishLoading({target: img});
+    } else {
+      setTimeout(checkSize, 100, img);
+    }
+  }
   data.forEach(function(elem) {
     var re = new RegExp(elem[0]), subs = elem[1];
     Instant.message.parser.addEmbedder(re, function(url) {
@@ -10,4 +25,15 @@ Instant.plugins.mailbox('embed-images').handle(function(data) {
       ]);
     }, {normalize: true});
   });
+  if (data.length)
+    Instant.message.parser.addProcessor(function(node) {
+      if (! node.classList.contains('in-chat')) return;
+      var images = $selAll('img.embed-image:not(.loading)', node);
+      Array.prototype.forEach.call(images, function(img) {
+        img.classList.add('loading');
+        img.addEventListener('load', finishLoading);
+        img.addEventListener('error', finishLoading);
+        checkSize(img);
+      });
+    });
 });
