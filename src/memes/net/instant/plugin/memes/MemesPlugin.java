8c599744c61472847835a9bc517c4bf8947d53d5
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-03-31 15:06:40 +0200

    [Plugins/memes] Commence meme mod

---
+++ src/memes/net/instant/plugin/memes/MemesPlugin.java
@@ -0,0 +1,12 @@
+package net.instant.plugin.memes;
+
+import net.instant.api.API1;
+import net.instant.api.PluginData;
+
+public class MemesPlugin {
+
+    public static void initInstantPlugin1(API1 api, PluginData data) {
+        api.handleDefault(data);
+    }
+
+}
