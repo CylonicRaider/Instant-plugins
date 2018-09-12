acb89aacb20a3fec05c70dfe4ef78d8a2482a96d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-09-12 19:55:51 +0200

    [Plugins/embed-example] Showcase inline embeds
    Also fixes a URL handling bug.

--- src/embed-example/static/embed-example.js
+++ src/embed-example/static/embed-example.js
@@ -2,8 +2,15 @@
 /* Example embedder for testing purposes */
 
 Instant.message.parser.addEmbedder(/^example:text\//, function(url) {
-  var text = decodeURI(url.substr(13)).replace(/\+/g, ' ');
+  var text = decodeURI(url.substr(13).replace(/\+/g, ' '));
   var node = Instant.message.parser.makeNode(text);
   node.style.fontFamily = 'serif';
   return node;
 });
+
+Instant.message.parser.addEmbedder(/^example:inline-text\//, function(url) {
+  var text = decodeURI(url.substr(20).replace(/\s+/g, ' '));
+  var node = Instant.message.parser.makeNode(text);
+  node.style.fontFamily = 'serif';
+  return node;
+}, {inline: true});
