ed925ffe0b01ccadb19fcdbef6a7a64fec034c4d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-08-08 23:19:17 +0200

    [Plugins/webrtc] Add user highlighting code

--- src/webrtc/static/webrtc.js
+++ src/webrtc/static/webrtc.js
@@ -1,2 +1,15 @@
 
 /* Instant video chat plugin functionality */
+
+Instant.webrtc = function() {
+  return {
+    /* Add or remove highlighting on this user list entry. */
+    _setHighlight: function(userNode, newState) {
+      if (newState) {
+        userNode.classList.add('highlight');
+      } else {
+        userNode.classList.remove('highlight');
+      }
+    }
+  };
+}();
