573ce0d6e31de59fa9ac59eefaf4f18ac1088986
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-08-10 23:37:34 +0200

    [Plugins/webrtc] Commence media capturing

--- src/webrtc/static/webrtc.js
+++ src/webrtc/static/webrtc.js
@@ -3,6 +3,21 @@
 
 Instant.webrtc = function() {
   return {
+    /* Create a media stream capture object covering audio and/or video from
+     * the user.
+     * The return value is a Promise, which may resolve, reject, or do
+     * neither. */
+    getUserMedia: function(audio, video) {
+      if (! navigator.mediaDevices) {
+        return Promise.reject(new Error('Media device API unavailable'));
+      }
+      try {
+        return navigator.mediaDevices.getUserMedia({audio: audio,
+                                                    video: video});
+      } catch (exc) {
+        return Promise.reject(exc);
+      }
+    },
     /* Add or remove highlighting on this user list entry. */
     _setHighlight: function(userNode, newState) {
       if (newState) {
