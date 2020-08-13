bf3085028c2a4b68f7d343ab87001cd5d22e044d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-08-13 21:06:36 +0200

    [Plugins/webrtc] Implement WebRTC negotiation

--- src/webrtc/static/webrtc.js
+++ src/webrtc/static/webrtc.js
@@ -39,6 +39,74 @@ Instant.webrtc = function() {
       }
       return ret;
     },
+    /* Create and return an RTCPeerConnection, setting it up to negotiate
+     * with a remote counterpart created by this function as well.
+     * setSignalFrom is a setter function that takes an event handler function
+     *               and installs it such that it is called whenever a
+     *               singaling datum from the counterpart arrives. This is
+     *               called while the _createConnection() call is underway,
+     *               exactly once.
+     * signalTo      is a function that takes singaling datum and relays it
+     *               the counterpart.
+     * peerFlag      is a Boolean that should be true on one peer and false on
+     *               the other. For example, the peer initiating the
+     *               connection could set this to true; then, the other peer
+     *               would set this to false.
+     * Returns an appropriately configured RTCPeerConnection object. */
+    _createConnection: function(setSignalFrom, signalTo, peerFlag) {
+      var ret = new RTCPeerConnection(configuration);
+      // Perfect (WebRTC) negotiation pattern as described on MDN. The code
+      // behaves "politely" iff peerFlag is true.
+      sendingOffer = false;
+      offerIgnored = false;
+      ret.addEventListener('negotiationneeded', function(event) {
+        // Whenever (re)negotiation is required, we update the local
+        // description and send it off.
+        sendingOffer = true;
+        ret.setLocalDescription().then(function() {
+          signalTo({type: 'description', data: ret.localDescription});
+        }).catch(function(err) {
+          console.error('WebRTC: Error while submitting local description:',
+                        err);
+        }).finally(function() {
+          sendingOffer = false;
+        });
+      });
+      ret.addEventListener('icecandidate', function(event) {
+        // Candidates are forwarded to the other side.
+        signalTo({type: 'candidate', data: event.candidate});
+      });
+      setSignalFrom(function(data) {
+        var promise;
+        if (data.type == 'description') {
+          // Incoming descriptions are subject to a collision check.
+          var description = data.data;
+          var collision = ((description.type == 'offer') &&
+            (sendingOffer || ret.signalingState != 'stable'));
+          offerIgnored = (collision && ! peerFlag);
+          if (offerIgnored) return;
+          // Accepted descriptions are passed on; offers are answered.
+          promise = pc.setRemoteDescription(description).then(function() {
+            if (description.type != 'offer') return;
+            return ret.setLocalDescription().then(function() {
+              signalTo({type: 'description', data: ret.localDescription});
+            });
+          });
+        } else if (data.type == 'candidate') {
+          // Candidates are fed back into WebRTC.
+          promise = pc.addIceCandidate(data.data);
+        } else {
+          return;
+        }
+        // Common error handler.
+        promise.catch(function(err) {
+          if (data.type == 'candidate' && offerIgnored) return;
+          console.error('WebRTC: Error while handling incoming ' + data.type +
+                        ':', err);
+        });
+      });
+      return ret;
+    },
     /* Add or remove highlighting on this user list entry. */
     _setHighlight: function(userNode, newState) {
       if (newState) {
