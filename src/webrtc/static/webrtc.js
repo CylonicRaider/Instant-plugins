99dc5ca1c8222ed1d3c04bbcfaaacd540a7e37d4
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-08-17 18:55:25 +0200

    [Plugins/webrtc] Add basic connection creation

--- src/webrtc/static/webrtc.js
+++ src/webrtc/static/webrtc.js
@@ -4,13 +4,17 @@
 Instant.webrtc = function() {
   /* The user identity. Stays stable while the page is loaded. */
   var identity = null;
-  /* Counter for connection ID-s. */
-  var idCounter = 1;
   /* The current configuration. */
   var configuration = {};
+  /* Connection storage. */
+  var connections = {};
   return {
     /* Initialize submodule. */
     init: function() {
+      Instant.connection.addHandler('p2p-signal', function(msg) {
+        Instant.webrtc._onmessage(msg);
+        return true;
+      });
       Instant.listen('identity.established', function(event) {
         if (identity == null) identity = Instant.identity.id;
       });
@@ -30,6 +34,13 @@ Instant.webrtc = function() {
     /* Retrieve the current RTCPeerConnection configuration object. */
     getRTCConfiguration: function() {
       return configuration;
+    },
+    /* Create a WebRTC connection to the given peer and return it. */
+    connectTo: function(peerID) {
+      var connID = Instant.webrtc._getConnectionID(peerID);
+      if (! connections[connID])
+        Instant.webrtc._createConnection(connID, peerID);
+      return connections[connID];
     },
     /* Create a media stream object capturing audio and/or video from the
      * user.
@@ -57,32 +68,54 @@ Instant.webrtc = function() {
       }
       return ret;
     },
-    /* Create and return an RTCPeerConnection, setting it up to negotiate
-     * with a remote counterpart created by this function as well.
+    /* Calculate the unique ID of any connection between this peer and the
+     * one named by peerID. */
+    _getConnectionID: function(peerID) {
+      if (peerID < identity) {
+        return peerID + ':' + identity;
+      } else {
+        return identity + ':' + peerID;
+      }
+    },
+    /* Create, install, and return a fully configured RTCPeerConnection.
+     * The connection has the given ID and communicates with the peer whose
+     * ID is given as well. */
+    _createConnection: function(connID, peerID) {
+      var ret = new RTCPeerConnection(configuration);
+      ret._instant = {id: connID, onSignalingInput: null};
+      Instant.webrtc._negotiate(ret, function(handler) {
+          ret._instant.onSignalingInput = handler;
+        }, function(data) {
+          Instant.connection.sendUnicast(peerID, {type: 'p2p-signal',
+            connection: connID, data: data});
+        }, peerFlag);
+      connections[connID] = ret;
+      return ret;
+    },
+    /* Configure the given RTCPeerConnection to negotiate with a counterpart
+     * calling this function as well.
      * setSignalFrom is a setter function that takes an event handler function
      *               and installs it such that it is called whenever a
      *               singaling datum from the counterpart arrives. This is
-     *               called while the _createConnection() call is underway,
-     *               exactly once.
+     *               called while the _negotiate() call is underway, exactly
+     *               once.
      * signalTo      is a function that takes singaling datum and relays it
      *               the counterpart.
      * peerFlag      is a Boolean that should be true on one peer and false on
      *               the other. For example, the peer initiating the
      *               connection could set this to true; then, the other peer
-     *               would set this to false.
-     * Returns an appropriately configured RTCPeerConnection object. */
-    _createConnection: function(setSignalFrom, signalTo, peerFlag) {
-      var ret = new RTCPeerConnection(configuration);
+     *               would set this to false. */
+    _negotiate: function(conn, setSignalFrom, signalTo, peerFlag) {
       // Perfect (WebRTC) negotiation pattern as described on MDN. The code
       // behaves "politely" iff peerFlag is true.
       sendingOffer = false;
       offerIgnored = false;
-      ret.addEventListener('negotiationneeded', function(event) {
+      conn.addEventListener('negotiationneeded', function(event) {
         // Whenever (re)negotiation is required, we update the local
         // description and send it off.
         sendingOffer = true;
-        ret.setLocalDescription().then(function() {
-          signalTo({type: 'description', data: ret.localDescription});
+        conn.setLocalDescription().then(function() {
+          signalTo({type: 'description', data: conn.localDescription});
         }).catch(function(err) {
           console.error('WebRTC: Error while submitting local description:',
                         err);
@@ -90,7 +123,7 @@ Instant.webrtc = function() {
           sendingOffer = false;
         });
       });
-      ret.addEventListener('icecandidate', function(event) {
+      conn.addEventListener('icecandidate', function(event) {
         // Candidates are forwarded to the other side.
         signalTo({type: 'candidate', data: event.candidate});
       });
@@ -100,14 +133,14 @@ Instant.webrtc = function() {
           // Incoming descriptions are subject to a collision check.
           var description = data.data;
           var collision = ((description.type == 'offer') &&
-            (sendingOffer || ret.signalingState != 'stable'));
+            (sendingOffer || conn.signalingState != 'stable'));
           offerIgnored = (collision && ! peerFlag);
           if (offerIgnored) return;
           // Accepted descriptions are passed on; offers are answered.
           promise = pc.setRemoteDescription(description).then(function() {
             if (description.type != 'offer') return;
-            return ret.setLocalDescription().then(function() {
-              signalTo({type: 'description', data: ret.localDescription});
+            return conn.setLocalDescription().then(function() {
+              signalTo({type: 'description', data: conn.localDescription});
             });
           });
         } else if (data.type == 'candidate') {
@@ -123,7 +156,27 @@ Instant.webrtc = function() {
                         ':', err);
         });
       });
-      return ret;
+    },
+    /* Handle an incoming (Instant client-to-client) message. */
+    _onmessage: function(msg) {
+      var data = msg.data;
+      switch (data.type) {
+        case 'p2p-signal':
+          var connID = data.connection;
+          if (! connID) {
+            console.warn('WebRTC: Invalid signaling message received:', msg);
+            break;
+          }
+          // Reception of a signaling event creates a new connection if
+          // necessary.
+          if (! connections[connID])
+            Instant.webrtc._createConnection(connID, msg.from);
+          connections[connID]._instant.onSignalingInput(data.data);
+          break;
+        default:
+          console.warn('WebRTC: Unknown client message?!', data);
+          break;
+      }
     },
     /* Add or remove highlighting on this user list entry. */
     _setHighlight: function(userNode, newState) {
